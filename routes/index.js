f9d4aae8502b4c2bf2b05a2e74b6cfc6384d1b9f
Mervin Choun <m.choun@gmail.com>
2023-02-14 19:11:55 -0500

    adding well known file for apple pay

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Braintree and PayPal Demo Index' });
 });
 
+router.get(
+  '/.well-known/apple-developer-merchantid-domain-association',
+  (req, res) => {
+    res.sendFile(resolve(__dirname, '../.well-known/domain-association-file-sandbox'))
+  }
+)
+
 module.exports = router;
