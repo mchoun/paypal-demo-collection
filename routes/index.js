60a48096872f4d53956073dcb81ad78bcc9d2bca
Mervin Choun <m.choun@gmail.com>
2023-04-06 15:35:17 -0400

    initial commit for folder restructure

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const braintreeRoutes = require('../products/braintree')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Braintree and PayPal Demo Index' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Braintree and PayPal Demo Index' })
+})
 
 router.get(
   '/.well-known/apple-developer-merchantid-domain-association',
   (req, res) => {
-    res.sendFile(resolve(__dirname, '../.well-known/domain-association-file-sandbox'))
+    res.sendFile(
+      resolve(__dirname, '../.well-known/domain-association-file-sandbox')
+    )
   }
 )
 
-module.exports = router;
+app.use('/braintree-examples', braintreeRoutes)
+app.use('/paypal-examples', paypalExamples)
+app.use('/api', apiRouter)
+app.use('/graqhql', graphql)
+
+module.exports = router
