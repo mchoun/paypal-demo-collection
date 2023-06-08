0422f8f21e9e967671f869b5e884de587300ae6c
Mervin Choun <m.choun@gmail.com>
2023-06-08 17:28:32 -0400

    educational testing

---
+++ test/import.js
@@ -0,0 +1,4 @@
+const { adam, mervin } = require('./export')
+
+console.log('This is adam', adam)
+console.log('This is mervin', mervin)
--- test/test.js
+++ test/test.js
@@ -1,7 +1,4 @@
 const braintree = require('braintree')
-require('dotenv').config()
-
-const { MERCHANT_ID, BT_PUBLIC_KEY, BT_PRIVATE_KEY } = process.env
 
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
@@ -10,36 +7,89 @@ const gateway = new braintree.BraintreeGateway({
   publicKey: '7d9ykdgnwypq55qg',
 })
 
-gateway.transaction
-  .sale({
-    amount: '10.00',
-    paymentMethodNonce: 'fake-valid-nonce',
-    options: {
-      submitForSettlement: true,
-    },
-  })
-  .then((result) => console.log(result))
+// function anotherReturnValue() {
+//   return 'Im string'
+// }
 
-async function tranasctionSaleAsync() {
-  const response = await gateway.transaction.sale({
-    amount: '10.00',
-    paymentMethodNonce: 'fake-valid-nonce',
-    options: {
-      submitForSettlement: true,
-    },
-  })
-
-  console.log(response)
+function callbackFunction(bananas, apples) {
+  console.log(bananas)
+  console.log(apples.id)
 }
 
-function tranasctionSale() {
-  const response = gateway.transaction.sale({
-    amount: '10.00',
+gateway.customer.create(
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    company: 'BT test Subs',
+    email: 'jdoe@bt.com',
+    phone: '412.892.7838',
+    fax: '906.995.9090',
+    website: 'www.jdoe.com',
     paymentMethodNonce: 'fake-valid-nonce',
-    options: {
-      submitForSettlement: true,
-    },
-  })
+  },
+  callbackFunction
+)
 
-  console.log(response)
-}
+// function returnValue() {}
+
+console.log(returnValue().then(callbackFunction))
+console.log(
+  returnValue().then((bananas, apples) => {
+    console.log(bananas)
+    console.log(apples.id)
+  })
+)
+
+// let clientToken = undefined
+
+// // async function getClientToken() {
+// //   try {
+// //     const clientTokenResponse = await gateway.clientToken.generate()
+// //   } catch {}
+// //   clientToken = clientTokenResponse.clientToken
+// //   const customerCreateResponse = await gateway.customer.create({
+// //     firstName: 'M',
+// //     lastName: 'C',
+// //     company: clientToken,
+// //   })
+
+// //   console.log(customerCreateResponse.success)
+// // }
+
+// getClientToken()
+
+// Fetch request?
+// https://developer.mozilla.org/en-US/docs/Web/API/fetch
+
+// fetch(`http://localhost:3000/api/test${clientToken}`, {
+//   method: 'POST',
+// })
+
+// gateway.clientToken.generate({}, (err, response) => {
+//   clientToken = response.clientToken
+//   console.log('Callback, baby!')
+// })
+
+// gateway.clientToken
+//   .generate({ customerId: 888888 })
+//   .then((clientTokenResponse) => {
+//     clientToken = clientTokenResponse.clientToken
+//     console.log("But then I'm a promise!")
+
+//     return clientToken
+//   })
+//   .then((clientToken) => {
+//     return gateway.customer.create({
+//       firstName: 'Mervin',
+//       lastName: 'Choun',
+//       company: clientToken,
+//     })
+//   })
+//   .then((response) => {
+//     console.log(response.success)
+//   })
+//   .catch((err) => console.log(err))
+
+// fetch(`http://localhost:3000/api/test${clientToken}`, {
+//   method: 'POST',
+// })
